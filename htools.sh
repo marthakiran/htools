849ebc30f80971d5039d1f86c3ca2470968969e3
marthakiran <krnkrn240@gmail.com>
2018-09-30 13:41:58 +0530

    URL of htool

---
+++ htools.sh
@@ -0,0 +1,37 @@
+# htools
+git clone https://github.com/secretsquirrel/the-backdoor-factory /opt/the-backdoor-factory
+git clone https://github.com/breenmachine/httpscreenshot.git /opt/httpscreenshot
+git clone https://github.com/brav0hax/smbexec.git /opt/smbexec
+apt-get install git gcc make libpcap-dev
+git clone https://github.com/robertdavidgraham/masscan.git /opt/masscan
+git clone https://github.com/Dionach/CMSmap /opt/CMSmap
+git clone https://github.com/wpscanteam/wpscan.git /opt/wpscan
+git clone https://github.com/ChrisTruncer/EyeWitness.git /opt/EyeWitness
+git clone https://github.com/MooseDojo/praedasploit /opt/praedasploit
+git clone https://github.com/sqlmapproject/sqlmap /opt/sqlmap
+git clone https://bitbucket.org/LaNMaSteR53/recon-ng.git /opt/recon-ng
+git clone https://github.com/leebaird/discover.git /opt/discover
+wget https://raw.github.com/beefproject/beef/a6a7536e/install-beef
+git clone https://github.com/SpiderLabs/Responder.git /opt/Responder
+git clone https://github.com/cheetz/Easy-P.git /opt/Easy-P
+git clone https://github.com/cheetz/Password_Plus_One /opt/Password_Plus_One
+git clone https://github.com/cheetz/PowerShell_Popup /opt/PowerShell_Popup
+git clone https://github.com/cheetz/icmpshock /opt/icmpshock
+git clone https://github.com/cheetz/brutescrape /opt/brutescrape
+git clone https://www.github.com/cheetz/reddit_xss /opt/reddit_xss
+git clone https://github.com/cheetz/PowerSploit /opt/HP_PowerSploit
+git clone https://github.com/cheetz/PowerTools /opt/HP_PowerTools
+git clone https://github.com/cheetz/nishang /opt/nishang
+git clone https://github.com/secforce/sparta.git /opt/sparta
+git clone https://github.com/tcstool/NoSQLMap.git /opt/NoSQLMap
+git clone https://github.com/trustedsec/social-engineer-toolkit/ /opt/set/
+git clone https://github.com/mattifestation/PowerSploit.git /opt/PowerSploit
+cd /opt/PowerSploit && wget https://raw.githubusercontent.com/obscuresec/random/master/StartListener.py && wget https://raw.githubusercontent.com/darkoperator/powershell_scripts/master/ps_encoder.py
+git clone https://github.com/samratashok/nishang /opt/nishang
+git clone https://github.com/Veil-Framework/Veil /opt/Veil
+git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
+git clone https://github.com/DanMcInerney/net-creds.git /opt/net-creds
+git clone https://github.com/derv82/wifite /opt/wifite
+git clone https://github.com/sophron/wifiphisher.git /opt/wifiphisher
+git clone https://github.com/pentestgeek/phishing-frenzy.git /var/www/phishing-frenzy
+git clone https://github.com/macubergeek/gitlist.git /opt/gitlist
